2560958d8d7721cfaea1a2ea86c73d046de9ab58
Jared Novack <jared@upstatement.com>
2014-12-01 15:25:13 -0500

    ref #121 -- $post->is_feature is method so it needs parens; don't need to separately get author data it comes for free with post.author

--- single.php
+++ single.php
@@ -8,17 +8,15 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
-
 $context = Timber::get_context();
 $post = Timber::query_post('HechingerPost');
 $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
-$context['author'] =  new TimberUser($post->post_author);
 // TODO: refactor to allow any arbitrary article type to load a template of that type
 if (post_password_required($post->ID)){
 	Timber::render('single-password.twig', $context);
-} elseif ( $post->is_feature ) {
+} elseif ( $post->is_feature() ) {
 	Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'pages/article-feature.twig'), $context);
 } else {
 	Timber::render(array('single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'pages/article.twig'), $context);

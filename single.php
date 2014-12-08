6ee24c9cc0b5aaf90eb23063095781c092da30db
Christopher Plummer <plumcakes@Christophers-MacBook-Pro.local>
2014-12-08 12:27:28 -0500

    Recirc now dynamic. Stats and pull quote zones dynamic. #192. Closes #199.

--- single.php
+++ single.php
@@ -10,9 +10,58 @@
  */
 $context = Timber::get_context();
 $post = Timber::query_post('HechingerPost');
+$homepage = Timber::get_post('home', 'HechingerPost');
 $context['post'] = $post;
 $context['wp_title'] .= ' - ' . $post->title();
 $context['comment_form'] = TimberHelper::get_comment_form();
+
+//TODO: filter out posts in homepage stream
+$context['latest_posts'] = Timber::get_posts(array('post_type' => 'post'), 'HechingerPost');
+
+if (class_exists('TimberStream')) {
+  $stream = new TimberStream('homepage');
+  $context['recirc_posts'] = $stream->get_posts(array(), 'HechingerPost');
+}
+
+$context['zquote'] = get_quote('z2_quote', $homepage);
+
+function get_quote( $qt_str, $homepage ) {
+  $zquote = array();
+  $zquote_link = $homepage->get_field($qt_str . '_link');
+  $zquote_text = $homepage->get_field($qt_str . '_text');
+  $zquote_subhead = $homepage->get_field($qt_str . '_subhead');
+  $zquote_number = $homepage->get_field($qt_str . '_number');
+
+  if ( isset($zquote_link) && is_array($zquote_link) ) {
+    $zquote['post'] = new HechingerPost($zquote_link[0]->ID);
+    $zquote['subhead'] = new HechingerPost($zquote_link[0]->ID);
+
+    if ( isset($zquote_text) && strlen($zquote_text) ) {
+      $zquote['text'] = $zquote_text;
+    } else {
+      $zquote['text'] = $zquote['post']->title;
+    }
+
+    if ( isset($zquote_subhead) && strlen($zquote_subhead) ) {
+      $zquote['subhead'] = $zquote_subhead;
+    } else {
+      $zquote['subhead'] = $zquote['post']->subhead;
+    }
+
+    if ( isset($zquote_number) && strlen($zquote_number) ) {
+      if ( strpos($zquote_number, '%') !== false) {
+        $zquote['number'] = substr($zquote_number, 0, -1) . '<span class="tz-special-precent">%</span>';
+      } else {
+        $zquote['number'] = $zquote_number;
+      }
+      $zquote['show_stat'] = true;
+    } else {
+      $zquote['show_stat'] = false;
+    }
+  }
+  return $zquote;
+}
+
 // TODO: refactor to allow any arbitrary article type to load a template of that type
 if (post_password_required($post->ID)){
 	Timber::render('single-password.twig', $context);

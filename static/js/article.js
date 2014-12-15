3118b6f5b19e2cf4dfe65d29d42212c3e6dcf08b
Christopher Plummer <chris.plummer@upstatement.com>
2014-12-15 18:16:23 -0500

    Sanity check js modules.

--- static/js/article.js
+++ static/js/article.js
@@ -1,6 +1,12 @@
 window.onload = (function($){
 
-  var quickshare = HE_quickShare.init();
-  var comments = HE_comments.init();
+  var quickshare, comments;
+
+  if (typeof HE_quickShare !== undefined && typeof HE_quickShare.init !== undefined) {
+    quickshare = HE_quickShare.init();
+  }
+  if (typeof HE_comments !== undefined && typeof HE_comments.init !== undefined) {
+    comments = HE_comments.init();
+  }
 
 }(jQuery));
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,13 @@
 window.onload = (function($){
 
-  var navbar = HE_navbar.init();
-  var responsiveBg = HE_responsiveBg.init();
+  var navbar, responsiveBg;
+
+  if (typeof HE_navbar !== undefined && typeof HE_navbar.init !== undefined) {
+    navbar = HE_navbar.init();
+  }
+
+  if (typeof HE_navbar !== undefined && typeof HE_responsiveBg.init !== undefined) {
+    responsiveBg = HE_responsiveBg.init();
+  }
 
 }(jQuery));

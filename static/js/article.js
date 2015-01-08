6b7598e656952f40daf46df28547e203b79d0988
Grant Staublin <grant.staublin@gmail.com>
2015-01-08 13:47:17 -0500

    Working on pull quotes. Work on #332.

--- static/js/article.js
+++ static/js/article.js
@@ -9,4 +9,28 @@ window.onload = (function($){
     comments = HE_comments.init();
   }
 
+
+  // Testing whether quote begins with a quote mark
+  if( $('.article-quote') ) {
+
+    var string1 = '"';
+    var string2 = "'";
+
+    $('.article-quote').each(function(i) {
+      var quoteContainer = $('.article-quote');
+      var theQuote = $(this).text();
+      var testQuote = $('.article-quote-h').text();
+
+      console.log(theQuote.substring(2,4) );
+      console.log(testQuote);
+
+      if ( theQuote.substring(2,4) === string2 ) {
+        $(this).addClass('is-quote');
+        console.log('i added a class');
+      }
+
+    });
+
+  }
+
 }(jQuery));

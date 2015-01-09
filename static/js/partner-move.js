c7a3351ed72641694d3398d610399c50581420fc
Christopher Plummer <chris.plummer@upstatement.com>
2015-01-09 11:11:16 -0500

    finish move partner script Fix js class names Rebase fix

--- static/js/partner-move.js
+++ static/js/partner-move.js
@@ -2,34 +2,31 @@ var HE_partner = (function($){
   "use strict";
 
   var $partnerAside;
-  var $paragraph;
+  var $targetParagraph;
 
   function init() {
-    $partnerAside = $('.partner-aside');
+    // get the partner aside
+    $partnerAside = $('.js-partner-aside');
     if (typeof $partnerAside === 'undefined' || !$partnerAside.length) {
       return;
     }
+    // find the third paragraph or the last paragraph if article less than 3 paragraphs
+    $targetParagraph = findParagraph(3);
 
-    $paragraph = findParagraph(3);
-
-    if (typeof $paragraph !== 'undefined' || !$paragraph || !$paragraph.length) {
-      return;
+    if (typeof $targetParagraph !== 'undefined' || !$targetParagraph || !$targetParagraph.length) {
+      $partnerAside.insertAfter($targetParagraph);
     }
   }
 
+  // find a paragraph by index, if article is shorter than index, find the last paragraph recursively
   function findParagraph(index) {
     var graph;
-
     if (index > 0) {
-      graph = $('.article-txt p:nth-child(' + index + ')');
+      graph = $('.js-article-text p:nth-child(' + index + ')');
       return graph !== 'undefined' && graph.length ? graph : findParagraph(index - 1);
     } else {
       return false;
     }
-  }
-
-  function insertPartners() {
-    $partnerAside.insertAfter($paragraph);
   }
 
   return {

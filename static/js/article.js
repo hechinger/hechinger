2536ae003ce0ac1b433a7256de65e3ad9c9ef277
Christopher Plummer <chris.plummer@upstatement.com>
2015-01-12 09:56:37 -0500

    Refactored js to modular pattern. linted js.

--- static/js/article.js
+++ static/js/article.js
@@ -8,29 +8,8 @@ window.onload = (function($){
   if (typeof HE_comments !== undefined && typeof HE_comments.init !== undefined) {
     comments = HE_comments.init();
   }
-
-
-  // Testing whether quote begins with a double or single quote mark
-  if( $('.js-article-quote-h').length ) {
-
-    var $quoteH = $('.js-article-quote-h');
-    var string1 = "\“";
-    var string2 = "\‘";
-
-    $quoteH.each(function(i) {
-      var $this = $(this);
-      var $parent = $this.parent();
-      var theQuote = $this.text();
-      var theTest = theQuote.substring(0,2);
-
-      if ( typeof theTest=="string" && theTest.match(string1) ) {
-        $parent.addClass('is-double-quote');
-      } else if( typeof theTest=="string" && theTest.match(string2)  ) {
-          $parent.addClass('is-single-quote');
-      }
-    });
-
-  } // article quote test
+  if (typeof HE_blockquote !== undefined && typeof HE_blockquote.init !== undefined) {
+    blockquote = HE_blockquote.init();
+  }
 
 }(jQuery));
-

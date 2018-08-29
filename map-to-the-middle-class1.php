859fbacd6de51fab623e5fe5c20fbd3293dd36f8
DM <mchenry@hechingerreport.org>
2018-08-29 16:14:45 -0400

    Map changes
    switching to special report template style

--- map-to-the-middle-class1.php
+++ map-to-the-middle-class1.php
@@ -1,14 +1,35 @@
-<?php 
+<?php
 
 /**
- * The template for Map to the Middle Class page.
+ * The template for displaying a single Special Report page.
  *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
- **/
+ * @package 	WordPress
+ * @subpackage 	Timber
+ * @since 		Timber 0.2
+ */
+
+global $paged;
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
 
 $context = Timber::get_context();
-$context['post'] = new HechingerPost();
-//Timber::render(array('pages/' . $context['post']->slug . '.twig', 'pages/page.twig'), $context);
-Timber::render( 'pages/map-to-the-middle-class1.twig', $context );
+$args = array(
+  'slug' => $params['name']
+);
+
+$term = Timber::get_terms('special-report', $args, 'HechingerTerm');
+
+if ( isset($term[0]) && get_class($term[0]) == 'HechingerTerm' ) {
+  $context['banner'] = $term[0]->get_image('yellow_pano');
+  $context['posts'] = $term[0]->get_posts(array('paged' => $paged), 'HechingerPost');
+  $context['term'] = $term[0];
+  $context['pagination'] = Timber::get_pagination();
+} else {
+  wp_redirect( '/index.php?s='. $params['name'], 301 );
+  exit;
+}
+
+$context['promos'] = HechingerSite::get_promos($context['term']->name);
+
+Timber::render('pages/special-report.twig', $context);

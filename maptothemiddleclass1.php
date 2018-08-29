407fd7a3e1f06a9ea2ee658002048abc20e4cc81
DM <mchenry@hechingerreport.org>
2018-08-29 13:57:01 -0400

    maptomiddleclass update
    Updating the map page

---
+++ maptothemiddleclass1.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * The template for Map to the Middle Class page.
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ **/
+
+$context = Timber::get_context();
+$context['post'] = new HechingerPost();
+//Timber::render(array('pages/' . $context['post']->slug . '.twig', 'pages/page.twig'), $context);
+Timber::render( 'pages/maptothemiddleclass1.twig', $context );
